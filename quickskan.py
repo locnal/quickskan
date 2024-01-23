fbbac2f4e95bfe888911f40be783bdff80e7bf0f
locnal <124815594+locnal@users.noreply.github.com>
2024-01-23 14:41:22 +0000

    Update quickskan.py
    - added whois lookup

--- quickskan.py
+++ quickskan.py
@@ -17,15 +17,27 @@ def dnslookup():
     except Exception as e:
         print(f"Error: {e}")
 
+
+def whois_look():
+    lookup = input("input domain / ip addr for whoislookup: ")
+    try:
+        response = subprocess.run(['whois', lookup], capture_output=True, text=True)
+        return response.stdout
+    except Exception as e:
+        print(f"Error: {e}")
+
 def quickskan():
     option = input("""Pick an option:
                         1. Port scan
                         2. dnslookup
+                        3. whois
                         """)
     if option == "1":
         return port_scan()
     elif option == "2":
         return dnslookup()
+    elif option == "3":
+        return whois_look()
     else:
         print("Option not picked/chosen. Rerun script") 
 
